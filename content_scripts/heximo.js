aa87f21787106c88ffb1a39c30a814e766f9fc0d
irrum6 <irrum6@github.com>
2021-11-14 14:10:33 +0400

    all buttons moved to sidebar removed dead code

--- content_scripts/heximo.js
+++ content_scripts/heximo.js
@@ -1,20 +1,6 @@
 console.log("Heximo");
 console.log(`Loaded on ${new Date()}`);
 
-browser.runtime.onMessage.addListener(msg => {
-    // console.log(msg);
-    if (msg.results !== undefined) {
-        console.log(msg.results);
-    }
-    if (msg.command !== undefined) {
-        let { command } = msg;
-        console.log(command);
-        if (command === "export") {
-            browser.runtime.sendMessage({ message: "24h" });
-        }
-    }
-});
-
 browser.commands.onCommand.addListener(function (command) {
     if (command === "_open_sidebar") {
         console.log("triggered");
--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,23 +1,76 @@
+let load_message = `SideBar was loaded on ${new Date().toString()}`;
+console.log(load_message);
+
 const q = s => document.body.querySelector(s);
 const on = "addEventListener";
 
-const readfile = () => {
+const error_log = err => console.error(err);
+
+const active_tabs = func => browser.tabs.query({ active: true, currentWindow: true }).then(func).catch(error_log);
+
+const do_search_history = async (period) => {
+    const validValues = ["24h", "30d", "12m", "all"];
+    //12m is equal to 365 days
+    if (!validValues.includes(period)) {
+        console.error("bad arguments - is either '24h', '30d', '12m' or 'all'");
+        return {};
+    }
+    let minus_date = 0;
+    let hour = 60 * 60 * 1000;
+    switch (period) {
+        case "24h":
+            minus_date = 24 * hour;
+            break;
+        case "30d":
+            minus_date = 24 * hour * 30;
+            break;
+        case "12m":
+            minus_date = 24 * hour * 365;
+            break;
+        case "all":
+            minus_date = 0;
+            break;
+        default:
+            //do nothing
+            break;
+    }
+    let startTime = new Date(Date.now() - minus_date);
+    let results = await browser.history.search({
+        "text": "",
+        "startTime": startTime
+    });
+    return results;
+};
+
+const do_export_history = async () => {
+    let period = "24h";
+    let results = await do_search_history(period);
+    const json_string = JSON.stringify(results);
+    const bytes = new TextEncoder().encode(json_string);
+    const bob = new Blob([bytes], {
+        type: "application/json;charset=utf-8"
+    });
+    let url = URL.createObjectURL(bob);
+    let filename = `history.${period}.json`;
+    browser.downloads.download({ url, filename, saveAs: true });
+};
+
+const do_import_history = () => {
     let file = document.body.querySelector("input[type=file]").files[0];
     let decoder = new TextDecoder();
     file.arrayBuffer().then(data => {
         let d = decoder.decode(data);
         let array = JSON.parse(d);
 
-        let title = "Test Title";
-
         for (const elem of array) {
             browser.history.addUrl({
                 url: elem.url,
-                title,
+                title: elem.title,
                 visitTime: elem.lastVisitTime
             });
         }
     });
 }
 
-q("button")[on]("click", readfile);
+q("#export_action")[on]("click", do_export_history);
+q("#import_action")[on]("click", do_import_history);
